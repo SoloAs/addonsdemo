cf24d6e7f7a2e1718ef30bca40879b96c439ad2e
AlexanderTyutyarev <alexander.tyutyarev@natcheztr.com>
2019-03-05 16:49:39 +0300

    experimental commit, untagged

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ console.log('vector 2 ', vec2) // Vector { x: 30, y: 0, z: 100 }
 
 const vecSum = vec1.add(vec2)
 console.log('vector sum ', vecSum) // Vector { x: 50, y: 10, z: 100 }
+console.log(process.memoryUsage().rss);
+
+ process.on('SIGINT', () => {
+   console.log(process.memoryUsage().rss);
+   process.exit()
+ });
